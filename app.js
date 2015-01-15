c75083978de783a94e180bb9683ac881f054f3db
tophat8855 <hrfarley@gmail.com>
2015-01-14 21:33:18 -0800

    setup vars

---
+++ app.js
@@ -0,0 +1,30 @@
+var display = document.getElementById('result_display_value');
+var opDisplay = document.getElementById('operator_display');
+var no1 = document.querySelectorAll('div#buttons_row_1 > div.button')[0];
+var no2 = document.querySelectorAll('div#buttons_row_1 > div.button')[1];
+var no3 = document.querySelectorAll('div#buttons_row_1 > div.button')[2];
+var plus = document.querySelectorAll('div#buttons_row_1 > div.button')[3];
+var no4 = document.querySelectorAll('div#buttons_row_2 > div.button')[0];
+var no5 = document.querySelectorAll('div#buttons_row_2 > div.button')[1];
+var no6 = document.querySelectorAll('div#buttons_row_2 > div.button')[2];
+var minus = document.querySelectorAll('div#buttons_row_2 > div.button')[3];
+var no7 = document.querySelectorAll('div#buttons_row_3 > div.button')[0];
+var no8 = document.querySelectorAll('div#buttons_row_3 > div.button')[1];
+var no9 = document.querySelectorAll('div#buttons_row_3 > div.button')[2];
+var times = document.querySelectorAll('div#buttons_row_3 > div.button')[3];
+var clear = document.querySelectorAll('div#buttons_row_4 > div.button')[0];
+var no0 = document.querySelectorAll('div#buttons_row_4 > div.button')[1];
+var enter = document.querySelectorAll('div#buttons_row_4 > div.button')[2];
+var dividedBy = document.querySelectorAll('div#buttons_row_4 > div.button')[3];
+
+
+// var button = document.getElementById('add_todo');
+// var textBox = document.getElementById('user_input');
+// var nextToDo = document.querySelectorAll("ul")[0];
+
+// button.addEventListener('click', function() {
+//   var newToDo = document.createElement('li');
+//   newToDo.innerHTML = textBox.value;
+//   nextToDo.appendChild(newToDo);
+//   textBox.value = '';
+// });
